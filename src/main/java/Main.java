4bd57f816e0ad4e4581299541f7b2b99f18fcc11
chenyunxing <yunxing.cyx@alibaba-inc.com>
2018-11-15 15:59:02 +0800

    refine log

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,41 +4,42 @@ import java.lang.instrument.ClassDefinition;
 import java.lang.instrument.Instrumentation;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.Date;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		System.out.println(">>>>> -------------- Class-ReLoader -------------- <<<<<");
+		log(">>>>> -------------- Class-ReLoader -------------- <<<<<");
 		String javaHome = System.getenv("JAVA_HOME");
 		if(javaHome == null || javaHome.equals("")) {
 			javaHome = System.getProperty("java.home");
 		}
-		System.out.println("Java Home is :" + javaHome);
+		log("Java Home is :" + javaHome);
 		String toolLib = (javaHome.endsWith("/") ? javaHome : javaHome + "/") + "lib/tools.jar";
-		System.out.println("tools.jar : " + toolLib);
+		log("tools.jar : " + toolLib);
 		if(!new File(toolLib).exists()) {
-			System.err.println("error : tools.jar not exist !");
+			logErr("error : tools.jar not exist !");
 			System.exit(1);
 		}
 		URLClassLoader url = new URLClassLoader(new URL[]{new URL("jar", "", "file:"+ toolLib + "!/")}, ClassLoader.getSystemClassLoader());
 		Thread.currentThread().setContextClassLoader(url);
 		try {
 			run(args[0], args[1], args[2]);
-			System.out.println("**reload Done.");
+			log("**reload Done.");
 		}catch (Throwable t) {
 			t.printStackTrace();
-			System.err.println("**reload Fail.");
+			logErr("**reload Fail.");
 		}
-		System.out.println(">>>>> -------------- Class-ReLoader -------------- <<<<<");
+		log(">>>>> -------------- Class-ReLoader -------------- <<<<<");
 	}
 
 	public static void agentmain(String agentArgs, Instrumentation inst) {
 		try {
-			System.out.println(">>>>> -------------- Class-ReLoader Agent -------------- <<<<<");
-			System.out.println("with Arguments :");
+			log(">>>>> -------------- Class-ReLoader Agent -------------- <<<<<");
+			log("with Arguments :");
 			System.out.println(agentArgs);
 			String[] clazz = agentArgs.split(",");
 			if(clazz.length != 2) {
-				System.err.println("Invalid arguments, expect : className,classFilePath");
+				logErr("Invalid arguments, expect : className,classFilePath");
 				return;
 			}
 			String clzName = clazz[0];
@@ -49,11 +50,11 @@ public class Main {
 				fi.read(clzFileContent);
 			}
 			inst.redefineClasses(new ClassDefinition(clz, clzFileContent));
-			System.out.println("Succeed.");
+			log("Succeed.");
 		}catch (Throwable t) {
 			t.printStackTrace();
 		}finally {
-			System.out.println(">>>>> -------------- Class-ReLoader Agent -------------- <<<<<");
+			log(">>>>> -------------- Class-ReLoader Agent -------------- <<<<<");
 		}
 	}
 
@@ -61,11 +62,20 @@ public class Main {
 		Class<?> vmClz = Thread.currentThread().getContextClassLoader().loadClass("com.sun.tools.attach.VirtualMachine");
 		Object vm = vmClz.getDeclaredMethod("attach", String.class).invoke(null, pid);
 		String agentJar = Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-		System.out.println("Agent jar : " + agentJar);
+		log("Agent jar : " + agentJar);
 		if(!new File(agentJar).exists()) {
-			System.err.println("Agent jar file not exist, please make sure it is in current dir");
+			logErr("Agent jar file not exist, please make sure it is in current dir");
 			return;
 		}
+		log("Target class File : " + clzFile);
 		vmClz.getDeclaredMethod("loadAgent", String.class, String.class).invoke(vm, agentJar, clz + "," + clzFile);
+	}
+
+	private static void log(String msg) {
+		System.out.println(new Date() + " >> " + msg);
+	}
+
+	private static void logErr(String msg) {
+		System.err.println(new Date() + " >> " + msg);
 	}
 }
